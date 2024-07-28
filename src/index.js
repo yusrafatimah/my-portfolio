bdb660bfc161e8b36ab6cf7c96fe2048be9b478e
Yusra <yusraafatima19@gmail.com>
2024-07-28 20:55:50 +0500

    App.js removed and MainBody element added into root

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainBody from './app/components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MainBody />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
